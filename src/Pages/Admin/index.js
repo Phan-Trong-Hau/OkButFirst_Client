4c8680eb618ce8b88ca9e79499bb104cfb018c9b
PhanTrongHau <haupk2003@gmail.com>
2023-04-11 13:32:02 +0700

    add logout for admin

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -2,8 +2,16 @@ import { useEffect } from "react";
 import "./Admin.scss";
 import Breadcrumb from "../../Components/Breadcrumb";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../../Context/AuthProvider";
 
 const Admin = () => {
+    const { setAuth } = useContext(AuthContext);
+
+    const handleLogout = () => {
+        setAuth("");
+    };
+
     useEffect(() => {
         document.title = "Admin";
     }, []);
@@ -38,6 +46,11 @@ const Admin = () => {
                                 </li>
                                 <li className="item">
                                     <Link to="/admin/blog">New Blog</Link>
+                                </li>
+                                <li className="item">
+                                    <Link to="#" onClick={handleLogout}>
+                                        Log Out
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
