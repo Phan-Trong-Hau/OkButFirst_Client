ed7ae4354c9f8dadbee9e2964e7225f089cc3c0a
PhanTrongHau <haupk2003@gmail.com>
2024-04-08 15:46:22 +0700

    add new users router

--- src/Pages/Admin/index.js
+++ src/Pages/Admin/index.js
@@ -6,59 +6,56 @@ import { useContext } from "react";
 import AuthContext from "../../Context/AuthProvider";
 
 const Admin = () => {
-    const { setAuth } = useContext(AuthContext);
+  const { setAuth } = useContext(AuthContext);
 
-    const handleLogout = () => {
-        setAuth("");
-    };
+  const handleLogout = () => {
+    setAuth("");
+  };
 
-    useEffect(() => {
-        document.title = "Admin";
-    }, []);
+  useEffect(() => {
+    document.title = "Admin";
+  }, []);
 
-    return (
-        <>
-            <main className="admin">
-                <section>
-                    <Breadcrumb breadcrumb="Admin" />
-                </section>
-                <section>
-                    <div className="container">
-                        <div className="admin-wrapper">
-                            <ul className="list">
-                                <li className="item">
-                                    <Link to="/admin/order">List Order</Link>
-                                </li>
-                                <li className="item">
-                                    <Link to="/admin/coffee-shop">
-                                        Coffee Shop
-                                    </Link>
-                                </li>
-                                <li className="item">
-                                    <Link to="/admin/merch-shop">
-                                        Merch Shop
-                                    </Link>
-                                </li>
-                                <li className="item">
-                                    <Link to="/admin/question">
-                                        Frequently Asked Questions
-                                    </Link>
-                                </li>
-                                <li className="item">
-                                    <Link to="/admin/blog">New Blog</Link>
-                                </li>
-                                <li className="item">
-                                    <Link to="#" onClick={handleLogout}>
-                                        Log Out
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main className="admin">
+        <section>
+          <Breadcrumb breadcrumb="Admin" />
+        </section>
+        <section>
+          <div className="container">
+            <div className="admin-wrapper">
+              <ul className="list">
+                <li className="item">
+                  <Link to="/admin/order">List Order</Link>
+                </li>
+                <li className="item">
+                  <Link to="/admin/coffee-shop">Coffee Shop</Link>
+                </li>
+                <li className="item">
+                  <Link to="/admin/merch-shop">Merch Shop</Link>
+                </li>
+                <li className="item">
+                  <Link to="/admin/question">Frequently Asked Questions</Link>
+                </li>
+                <li className="item">
+                  <Link to="/admin/blog">New Blog</Link>
+                </li>
+                <li className="item">
+                  <Link to="/admin/users">Users</Link>
+                </li>
+                <li className="item">
+                  <Link to="#" onClick={handleLogout}>
+                    Log Out
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Admin;
