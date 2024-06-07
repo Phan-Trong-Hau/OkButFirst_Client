9b0632e2a7f84406edae99c3837768a6fcbb4b79
PhanTrongHau <haupk2003@gmail.com>
2024-06-07 20:52:37 +0700

    updated component popup

--- src/Components/PopUp/index.js
+++ src/Components/PopUp/index.js
@@ -1,28 +1,29 @@
-import './PopUp.scss'
+import "./PopUp.scss";
 
-const PopUp = ({setPopUp , message}) => {
+const PopUp = ({ setPopUp, message, children }) => {
+  const handlePopUp = () => {
+    setPopUp(false);
+  };
+  return (
+    <>
+      <div className="popup">
+        <div className="popup-box">
+          <div className="popup-content">
+            {!children && (
+              <>
+                <div className="desc">{message}</div>
+                <button className="theme-btn__black" onClick={handlePopUp}>
+                  OK
+                </button>
+              </>
+            )}
+            {children}
+          </div>
+        </div>
+        <div className="popup-background" onClick={handlePopUp}></div>
+      </div>
+    </>
+  );
+};
 
-    const handlePopUp = () => {
-        setPopUp(false);
-    }
-    return (
-        <>
-            <div className="popup">
-                <div className="popup-box">
-                    <div className="popup-content">
-                        <div className="desc">{message}</div>
-                        <button
-                            className="theme-btn__black"
-                            onClick={handlePopUp}
-                        >
-                            OK
-                        </button>
-                    </div>
-                </div>
-                <div className="popup-background" onClick={handlePopUp}></div>
-            </div>
-        </>
-    );
-}
- 
 export default PopUp;
